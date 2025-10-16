9ada1535266e4c255722a7b561e0fc394fab61b6
tigmen <tigmenal@gmail.com>
2025-10-16 22:28:21 +0300

    Добавил функцию для загрузки из файла

--- config.h
+++ config.h
@@ -15,9 +15,12 @@ typedef uint8_t byte;
 typedef uint16_t word;
 typedef word address;
 
-extern address mem[(address)(-1)];
+extern byte mem[(address)(-1)];
 
 void b_write(address, byte);
 byte b_read(address);
 void w_write(address, word);
 word w_read(address);
+
+void load_file(char*);
+void print_segment(address, int);
--- memmory.c
+++ memmory.c
@@ -1,5 +1,6 @@
-#include "config.h"
 #include <assert.h>
+#include <stdlib.h>
+#include "config.h"
 
 void b_write(address adr, byte src) {
         mem[adr] = src;
@@ -10,21 +11,40 @@ byte b_read(address adr) {
 }
 
 void w_write(address adr, word src) {
-	assert(adr&1);
+        assert(~adr & 1);
 
-	mem[adr] = src & BYTEMASK;
-	src >>= BYTESIZE;
-	mem[adr + 1] src & BYTEMASK;
+        b_write(adr, src & BYTEMASK);
+        src >>= BYTESIZE;
+        b_write(adr + 1, src & BYTEMASK);
 }
 
 word w_read(address adr) {
-	assert(adr&1);
+        assert(~adr & 1);
 
-        word w = mem[adr + 1];
+        word w = b_read(adr + 1);
         w <<= BYTESIZE;
-        w |= mem[adr];
+        w |= b_read(adr);
 
         return w & WORDMASK;
 }
 
-	
+void load_file(char* path) {
+        FILE* script = fopen(path, "r");
+        if (script == NULL) {
+                perror("\033[1;31mNo script file\033[0m");
+                exit(1);
+        }
+
+        address adr;
+        address n;
+        while (fscanf(script, "%" PRIadr "%d" PRIadr, &adr, &n) == 2) {
+                for (int i = 0; i < n; i++) {
+                        byte input;
+                        fscanf(script, "%o", &input);
+                        b_write(adr + i, input);
+                }
+        }
+
+        fclose(script);
+        return;
+}
