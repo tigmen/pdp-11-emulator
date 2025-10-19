076047708d5b0ff7088ac9865c201c6acde48c62
tigmen <tigmenal@gmail.com>
2025-10-19 00:59:22 +0000

    Bugfix функции чтения из файла Добавил функцию для печати n байт начиная с некого адреса

--- config.h
+++ config.h
@@ -10,8 +10,10 @@
 
 #define BYTEMASK 0xFF
 #define BYTESIZE 8
+#define BYTE2POWER 2 
 #define WORDMASK 0xFFFF
 #define WORDSIZE 16
+#define WORD2POWER 4
 
 typedef uint8_t byte;
 typedef uint16_t word;
@@ -20,12 +22,10 @@ typedef word address;
 extern char X[16];
 extern byte mem[(address)(-1)];
 
-uint16_t x_read_uint16(FILE *);
-
 void b_write(address, byte);
 byte b_read(address);
 void w_write(address, word);
 word w_read(address);
 
-void load_file(char*);
-void print_segment(address, int);
+void load_file(FILE*);
+void print_segment(address, address);
--- memmory.c
+++ memmory.c
@@ -1,24 +1,6 @@
 #include <assert.h>
 #include "config.h"
 
-char X[] = "0123456789abcdef";
-uint16_t x_read_uint16(FILE* stream) {
-	uint16_t result = 0;
-
-	char c = fgetc(stream);
-	char* pc = strchr(X, c);
-	/* while(!(pc) && pc != NULL) { */
-	/*	c = fgetc(stream); */
-	/* } */
-	while (pc = strchr(X, c)) {
-		result *= 16;
-		result += (pc - X);
-		c = fgetc(stream);
-	}
-
-	return result;
-}
-
 void b_write(address adr, byte src) {
 	mem[adr] = src & BYTEMASK;
 }
@@ -45,26 +27,61 @@ word w_read(address adr) {
 	return w & WORDMASK;
 }
 
-void load_file(char* path) {
-	FILE* script = fopen(path, "r");
+char X[] = "0123456789abcdef";
+address toAddress(char* src) {
+	char* p = src;
+	address result = 0;
+
+	while (*p++) {
+		result *= 16;
+		result += (strchr(X, p[-1]) - X);
+	}
+
+	return result;
+}
+byte toByte(char* src) {
+	char* p = src;
+	byte result = 0;
+
+	while (*p++) {
+		result *= 16;
+		result += (strchr(X, p[-1]) - X);
+	}
+
+	return result;
+}
+
+void load_file(FILE* script) {
 	if (script == NULL) {
 		perror("\033[1;31mNo script file\033[0m");
 		exit(1);
 	}
 
-	char buf;
-	while ((buf = fgetc(script)) != EOF) {
-		fseek(script, -1, SEEK_CUR);
+	char input[WORD2POWER + 1];
+	while (fgets(input, sizeof(input), script)) {
+		fgetc(script);
+		address adr = toAddress(input);
 
-		address adr = x_read_uint16(script);
-		address n = x_read_uint16(script);
+		fgets(input, sizeof(input), script);
+		fgetc(script);
+		address n = toAddress(input);
 
-		for (address i = 0; i < n; i++) {
-			byte input = x_read_uint16(script);
-			b_write(adr + i, input);
+		char x[BYTE2POWER + 1];
+		for (unsigned i = 0; i < n; i++) {
+			fgets(x, sizeof(x), script);
+			fgetc(script);
+			b_write(adr + i, toByte(x));
 		}
 	}
 
-	fclose(script);
+	return;
+}
+
+void print_segment(address adr, address n) {
+	for(int i = 0; i < n; i+=2) {
+		word line = w_read(adr + i);
+		printf("%06o: %06o %04x\n", adr + i, line, line);
+	}
+
 	return;
 }
